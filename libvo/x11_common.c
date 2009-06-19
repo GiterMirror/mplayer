GiterMirror/mplayer

commit 4cc23b0b34e3459fe30b43336d35efe8bc7d734a

Only set VO_EVENT_RESIZE if size actually changed, not if e.g. the window was only moved to the foreground.
git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28541 b3059339-0415-0410-9bf9-f77b7e298cf2

libvo/x11_common.c
@@ -844,8 +844,12 @@ int vo_x11_check_events(Display * mydisplay)
 //         if (vo_fs && Event.xconfigure.width != vo_screenwidth && Event.xconfigure.height != vo_screenheight) break;
                 if (vo_window == None)
                     break;
-                vo_x11_update_geometry();
-                ret |= VO_EVENT_RESIZE;
+                {
+                    int old_w = vo_dwidth, old_h = vo_dheight;
+                    vo_x11_update_geometry();
+                    if (vo_dwidth != old_w || vo_dheight != old_h)
+                        ret |= VO_EVENT_RESIZE;
+                }
                 break;
             case KeyPress:
                 {

commit 1ef58998e0dda56648512fcfc2f78b2916d6cb35

fix missing event on move that breaks xmga window movement broken in r28541

libvo/x11_common.c
@@ -846,8 +846,9 @@ int vo_x11_check_events(Display * mydisplay)
                     break;
                 {
                     int old_w = vo_dwidth, old_h = vo_dheight;
+		    int old_x = vo_dx, old_y = vo_dy;
                     vo_x11_update_geometry();
-                    if (vo_dwidth != old_w || vo_dheight != old_h)
+                    if (vo_dwidth != old_w || vo_dheight != old_h || vo_dx != old_x || vo_dy != old_y)
                         ret |= VO_EVENT_RESIZE;
                 }
                 break;