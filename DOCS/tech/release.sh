bf69d3e57fd154677ffea0fd2f2a3567328e382b
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-05-05 18:03:28 +0000

    Update release script for 1.1 branch.

--- DOCS/tech/release.sh
+++ DOCS/tech/release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # will create checkouts and tarballs in the current dir
-ver=1.1
+ver=1.1.1
 dst=MPlayer-$ver
 #svnurl=svn://svn.mplayerhq.hu/mplayer/trunk
-svnurl=svn://svn.mplayerhq.hu/mplayer/branches/$ver
+svnurl=svn://svn.mplayerhq.hu/mplayer/branches/1.1
 
 rm -rf $dst/ $dst-DOCS/ $dst.tar*
 
@@ -25,7 +25,7 @@ mv $dst-DOCS/DOCS/HTML $dst/DOCS
 rm -rf $dst-DOCS/
 
 # git archive unfortunately is refused
-git clone --depth 1 git://git.videolan.org/ffmpeg.git $dst/ffmpeg
+git clone --depth 1 -b release/0.11 git://git.videolan.org/ffmpeg.git $dst/ffmpeg
 rm -rf $dst/.git* $dst/ffmpeg/.git*
 
 # create .tar.gz and .tar.xz files
